943242146c5d24c3cdd23a0712982e8bfe0a00e5
ndanel <ndanel@la-providence.net>
2021-07-11 02:24:37 +0200

    � init

---
+++ commands/moderation.js
@@ -0,0 +1,59 @@
+const config = require('../config.json');
+
+module.exports = {
+    name:'kick',
+    description:'Cette commande kick un utilisateur !',
+    kickUser: function(msg)
+    {
+        const member = msg.mentions.users.first();
+        if(member){
+            let target = msg.guild.members.cache.get(member.id);
+            const reason = msg.content.split(" ").slice(2).join(' ');
+            target.kick(reason);
+
+            target = msg.mentions.users.first().username;    
+            msg.channel.send("L'Utilisateur " + target + " a bien été kick pour le motif suivant : " + reason);
+        }else{
+            msg.channel.send("Impossible de kick cet utilisateur !");
+        }
+    },
+
+    name:'ban',
+    description:'Cette commande ban un utilisateur !',
+    banUser: function(msg)
+    {
+        const member = msg.mentions.users.first();
+        if(member){
+            let target = msg.guild.members.cache.get(member.id);
+            const reason = msg.content.split(" ").slice(2).join(' ');
+            target.ban({reason});
+
+            target = member.username;    
+            msg.channel.send("L'Utilisateur " + target + " a bien été ban pour le motif suivant : " + reason);
+        }else{
+            msg.channel.send("Impossible de ban cet utilisateur");
+        }
+    },
+
+    name:'renew',
+    description:'Cette commande renew un salon !',
+    renewChannel: async function(msg)
+    {
+        const oldChannel = msg.channel;
+        const newChannel = await msg.channel.clone();
+
+        newChannel.setPosition(oldChannel.position);
+        oldChannel.delete();
+    },
+
+    name:'prefix',
+    description:'Cette commande change le préfix du bot !',
+    newPrefix: function(msg)
+    {
+        
+        const param = msg.content.split(" ").slice(1);
+        config.prefixCmd = param;
+
+        msg.channel.send("Le nouveau préfixe est : " + param);
+    }
+}
---
+++ index.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+const moderation = require('./commands/moderation');
+
+//Toutes les actions à faire quand le bot se connecte
+client.on("ready", function () {
+    console.log("Mon BOT est Connecté");
+    client.user.setActivity(".gg/infoschool", {type:'WATCHING'});
+})
+
+function invalidCommand(msg)
+{
+    msg.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+}
+// Répondre à un message
+client.on("message", msg => {
+    // Si le message n'est pas préfixé il est ignoré
+    if(!msg.content.startsWith(config.prefixCmd) || msg.author.bot) return
+
+    // Split string et préfixe
+    const args = msg.content.slice(config.prefixCmd.length).trim().split(/ +/);
+    // Extraction commande
+    const command = args.shift().toLowerCase();
+    // Extraction paramètres
+    //const param = msg.content.split(" ").slice(2);
+
+    //Commandes utilisateurs
+    switch(command)
+    {
+        case "ping":
+            msg.channel.send("Pong");
+            break;
+        case "kick":
+            if(!msg.member.hasPermission("KICK_MEMBERS")) return invalidCommand(msg)
+            moderation.kickUser(msg);
+            break;
+        case "ban":
+            if(!msg.member.hasPermission("BAN_MEMBERS")) return invalidCommand(msg)
+            moderation.banUser(msg);
+            break;
+        case "renew":
+            if(!msg.member.hasPermission("MANAGE_CHANNELS")) return invalidCommand(msg)
+            moderation.renewChannel(msg);
+            break;
+        case "prefix":
+            if(!msg.member.hasPermission("ADMINISTRATOR")) return invalidCommand(msg)
+            moderation.newPrefix(msg);
+            break;
+        default:
+            msg.channel.send("Taper !help pour la liste des commandes !");
+            break;
+    }
+})
+
+client.login(config.token);
